e45d432520766eaf11c6e2a6608ed797627d2dfc
myungho <skqoaudgh2@hanmail.net>
2020-05-06 16:01:46 +0900

    Context와 Context Provider 추가

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,22 @@
+import React, { Component, createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+  state = {
+    isLightTheme: true,
+    light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
+    dark: { syntax: '#ddd', ui: '#333', bg: '#555' },
+  };
+  render() {
+    return (
+      <ThemeContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+export default ThemeContextProvider;
+
+// Provider의 하위 컴포넌트들은 Provider의 value의 값을 참조할 수 있다.
