b421bc956f8f36c82984c86b6cf2ac8484577586
myungho <skqoaudgh2@hanmail.net>
2020-05-07 15:58:14 +0900

    Context Provider를 함수형 컴포넌트에서 사용

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
   return (
     <div
@@ -10,9 +12,13 @@ const BookList = () => {
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>The way of kings</li>
-        <li style={{ background: theme.ui }}>The name of the wind</li>
-        <li style={{ background: theme.ui }}>The final empire</li>
+        {books.map((book) => {
+          return (
+            <li style={{ background: theme.ui }} key={book.id}>
+              {book.title}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
