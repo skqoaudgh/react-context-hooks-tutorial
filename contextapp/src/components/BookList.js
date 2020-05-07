e9e07cd50f6f9e6d5d686787d082a7456067fd61
myungho <skqoaudgh2@hanmail.net>
2020-05-07 15:41:31 +0900

    Hooks - useContext 사용
    useContext를 통해 함수형 컴포넌트에서 쉽게 Context를 사용할 수 있다.

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <div
-              className="book-list"
-              style={{ background: theme.bg, color: theme.syntax }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>The way of kings</li>
-                <li style={{ background: theme.ui }}>The name of the wind</li>
-                <li style={{ background: theme.ui }}>The final empire</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The way of kings</li>
+        <li style={{ background: theme.ui }}>The name of the wind</li>
+        <li style={{ background: theme.ui }}>The final empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
